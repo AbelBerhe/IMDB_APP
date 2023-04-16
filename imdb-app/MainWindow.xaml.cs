6fb93ac21b49bc0ba1ab3c43a60fe39d53825d07
ZackGlenen <zackglenen@gmail.com>
2023-04-16 15:50:35 -0300

    Init mainframe & all pages

--- imdb-app/MainWindow.xaml.cs
+++ imdb-app/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace imdb_app
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public Pages.HomePage HomePage { get; set; }
+        public Pages.DirectorPage DirectorPage { get; set; }
+        public Pages.FilmPage FilmPage { get; set; }
+        public Pages.ShortsPage ShortsPage { get; set; }
+
+
         public MainWindow()
         {
             InitializeComponent();
+            HomePage = new Pages.HomePage();
+            DirectorPage = new Pages.DirectorPage();
+            FilmPage = new Pages.FilmPage();
+            ShortsPage = new Pages.ShortsPage();
+            mainFrame.NavigationService.Navigate(HomePage);
+        }
+
+        private void home_btn_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(HomePage);
+
+        }
+
+        private void director_btn_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(DirectorPage);
+
+        }
+
+        private void film_btn_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(FilmPage);
+
+        }
+
+        private void shorts_btn_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(ShortsPage);
+
         }
     }
 }
---
+++ imdb-app/Pages/ShortsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace imdb_app.Pages
+{
+    /// <summary>
+    /// Interaction logic for ShortsPage.xaml
+    /// </summary>
+    public partial class ShortsPage : Page
+    {
+        public ShortsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
