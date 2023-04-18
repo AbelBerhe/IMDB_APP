e78e95aff7ca3d60cd820feff2ac548b015e4f98
michaelatamosa <90644211+michaelatamosa@users.noreply.github.com>
2023-04-17 23:23:38 -0300

    designchanges

--- imdb-app/Pages/HomePage.xaml.cs
+++ imdb-app/Pages/HomePage.xaml.cs
@@ -46,8 +46,6 @@ namespace imdb_app.Pages
                     Year = nameGroup.Select(t => t.title.formattedYear).FirstOrDefault(),
                     Time = nameGroup.Select(t => t.title.formattedTime).FirstOrDefault(),
                     Rating = nameGroup.Select(t => t.rating.formattedRating).FirstOrDefault(),
-                    Genres = nameGroup.Select(t => t.title.Genres).FirstOrDefault()
-
                 };
 
             homeViewSource.Source = query.Take(3).ToList();
