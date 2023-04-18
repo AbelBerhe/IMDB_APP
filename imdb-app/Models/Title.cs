37873ac13f77f0f183196769e9aee1741b4f3e52
JayCleroux <w0270185@nscc.ca>
2023-04-18 00:18:33 -0300

    formatting to directors

--- imdb-app/Models/Title.cs
+++ imdb-app/Models/Title.cs
@@ -1,11 +1,4 @@
-﻿using IMDB.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IMDB.Models
+﻿namespace IMDB.Models
 {
     public partial class Title
     {
@@ -22,29 +15,6 @@ namespace IMDB.Models
             get
             {
                 return "Year: " + StartYear.ToString();
-            }
-        }
-
-        public int formattedGenre
-        {
-            get
-            {
-                var gen = 0;
-
-                foreach (Genre genre in Genres)
-                {
-                    gen += 1;
-                }
-                return gen;
-            }
-        }
-
-
-               foreach (Genre genre in Genres)
-                {
-                    gen += 1;
-                }
-               return gen;
             }
         }
 
